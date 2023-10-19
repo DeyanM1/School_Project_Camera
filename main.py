9f44613039148ba649db7bd343558abed849187f
DeyanM1 <de.mrkv@gmail.com>
2023-10-19 21:24:55 +0200

    Added Google Drive
    Connect to google drive function and updated requirements.txt

--- main.py
+++ main.py
@@ -4,11 +4,38 @@ import numpy as np
 from pyzbar.pyzbar import decode
 import time
 
+import pygsheets
+import pandas as pd
 
 list_monday = ["wwwwww", "iiiii", "wasd"]
 list_new = []
 for elem in list_monday:    
     list_new.append(elem)
+
+
+def connectToGoogleDrive():
+    
+    #authorization
+    gc = pygsheets.authorize(service_file='/Users/erikrood/desktop/QS_Model/creds.json')
+
+    # Create empty dataframe
+    df = pd.DataFrame()
+
+    # Create a column
+    df['name'] = ['John', 'Steve', 'Sarah']
+
+    #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+    sh = gc.open('PY to Gsheet Test')
+
+    #select the first sheet 
+    wks = sh[0]
+
+    #update the first sheet with df, starting at cell B2. 
+    wks.set_dataframe(df,(1,1))
+
+
+
+
 
 
 print("Searching cam") 
